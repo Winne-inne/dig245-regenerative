8df67ad64a9fc3da8bc4f8cef6ad96da85f0d7db
Winne-inne <abjeanbaptiste@davidson.edu>
2024-10-21 01:00:21 -0400

    prototype/
    test buttons/ added cats

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,29 @@
 
 /* javascript */
+
+let fur =["black", "orange", "pink"]
+let tail =["gray", "cream", "pink"]
+let rest =["window", "fire", "books"]
+
+function fur_pick(color){
+  let bod= document.querySelector(".body");
+  if(color == "black"){
+    console.log("black works");
+    bod.innerHTML = "<img src= assets/img/black_cat.PNG>"
+    
+  }
+  else if (color == "orange"){
+    console.log("orange works");
+    bod.innerHTML = "<img src= assets/img/orange_cat.PNG>";
+    
+  }
+  
+};
+
+
+/*** event listeners */
+
+document.getElementById("black_fur").addEventListener("click", fur_pick("black"));
+document.getElementById("orange_fur").addEventListener("click", fur_pick("orange"));
+
+  
