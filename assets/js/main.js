5c4ae918b65a2fe942dbdcbedb31d1ae40f05dde
Winne-inne <abjeanbaptiste@davidson.edu>
2024-10-23 15:39:51 -0400

    Added all the cat fur colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,40 @@
 
 /* javascript */
 
-let fur =["black", "orange", "pink"]
-let tail =["gray", "cream", "pink"]
-let rest =["window", "fire", "books"]
+let fur = ["black", "orange", "pink"]
+let tail = ["gray", "cream", "pink"]
+let rest = ["window", "fire", "books"]
 
-function fur_pick(color){
-  let bod= document.querySelector(".body");
-  if(color == "black"){
+function fur_pick(color) {
+  let bod = document.querySelector(".body");
+  if (color == "black") {
     console.log("black works");
     bod.innerHTML = "<img src= assets/img/black_cat.PNG>"
-    
+
   }
-  else if (color == "orange"){
+  else if (color == "orange") {
     console.log("orange works");
     bod.innerHTML = "<img src= assets/img/orange_cat.PNG>";
-    
+
   }
-  
+  else if (color == "pink") {
+    console.log("pink works");
+    bod.innerHTML = "<img src= assets/img/pink_cat.PNG>";
+
+  }
+
 };
 
 
 /*** event listeners */
 
-document.getElementById("black_fur").addEventListener("click", fur_pick("black"));
-document.getElementById("orange_fur").addEventListener("click", fur_pick("orange"));
+document.getElementById("black_fur").addEventListener("click", function () {
+  fur_pick("black")
+});
+document.getElementById("orange_fur").addEventListener("click", function () {
+  fur_pick("orange")
+});
+document.getElementById("pink_fur").addEventListener("click", function () {
+  fur_pick("pink")
+});
 
-  
